a5645ae013d990bf69ccfabc738560e5f46ca9b9
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2017-12-21 21:19:03 -0800

    Update parser.js

--- schedule/parser.js
+++ schedule/parser.js
@@ -2,26 +2,21 @@ $('#load').click(function () {
   var input = $('#data').val();
   var schedule = JSON.parse(input);
   if (schedule.type === 'static') {
-    (function () {
-      var display = '';
-      var i;
-      for (i = 0; i < schedule.times.length; i++) {
-        display += '<tr><td>' + schedule.times[i].name + '</td><td>' + schedule.times[i].start + '</td><td>' + schedule.times[i].end + '</td>';
-      }
-      $('#display').empty();
-      $('#display').append(display);
-    })();
-    $('#pre').hide();
-    $('#main').show();
+    loadTable(schedule.times);
   } else if (schedule.type === 'oddEven') {
-    (function () {
-      var display = '';
-      var i;
-      var day = new Date().getDay() % 2;
-      var scheduleToday = schedule.times[day];
-      for (i = 0; i < scheduleToday.length; i++) {
-        display += '<tr><td>' + scheduleToday[i].name + '</td><td>' + scheduleToday[i].start + '</td><td>' + scheduleToday[i].end + '</td>';
-      }
-    })();
+    var day = new Date().getDay() % 2;
+    var scheduleToday = schedule.times[day];
+    loadTable(schedule.times[day]);
   }
 });
+
+function loadTable (data) {
+  var display = '';
+  var i;
+  for (i = 0; i < data.length; i++) {
+    display += '<tr><td>' + data[i].name + '</td><td>' + data[i].start + '</td><td>' + data[i].end + '</td></tr>';
+  }
+  $('#display').html(display);
+  $('#pre').hide();
+  $('#main').show();
+}
