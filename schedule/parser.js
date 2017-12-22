699a0b3d479deb3dfe8af2d05f0ffdc278dcbd39
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2017-12-21 21:04:18 -0800

    Update parser.js

--- schedule/parser.js
+++ schedule/parser.js
@@ -1,1 +1,17 @@
-
+$('#load').click(function () {
+  var input = $('#data').val();
+  var schedule = JSON.parse(input);
+  if (schedule.type === 'static') {
+    (function () {
+      var display = '';
+      var i;
+      for (i = 0; i < schedule.times.length; i++) {
+        display += '<tr><td>' + schedule.times[i].name + '</td><td>' + schedule.times[i].start + '</td><td>' + schedule.times[i].end + '</td>';
+      }
+      $('#display').empty();
+      $('#display').append(display);
+    })();
+    $('#pre').hide();
+    $('#main').show();
+  }
+});
