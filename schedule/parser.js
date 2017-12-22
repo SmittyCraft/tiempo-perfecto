a346199e75dfacc88be5f3d46e518466fe47872c
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2017-12-22 10:51:05 -0800

    Update parser.js

--- schedule/parser.js
+++ schedule/parser.js
@@ -5,19 +5,34 @@ $('#load').click(function () {
   var schedule = JSON.parse(input);
   if (schedule.type === 'static') {
     events = schedule.times;
-    loadTable(events);
   } else if (schedule.type === 'oddEven') {
     var day = new Date().getDay() % 2;
     events = schedule.times[day];
-    loadTable(events);
+  } else {
+    return;
   }
+  loadTable(events);
 });
+
+function parseHour (hora) {
+  var hour = parseInt(hora.substr(0, 2), 10);
+  if (hour > 12) {
+    return hour - 12 + ' PM';
+  } else if (hour === 0) {
+    return '12 AM';
+  } else {
+    return hour + ' AM';
+  }
+}
 
 function loadTable (data) {
   var display = '';
   var i;
   for (i = 0; i < data.length; i++) {
-    display += '<tr><td>' + data[i].name + '</td><td>' + data[i].start + '</td><td>' + data[i].end + '</td></tr>';
+    var name = data[i].name;
+    var start = parseHour(data[i].start);
+    var end = parseHour(data[i].end);
+    display += '<tr><td>' + data[i].name + '</td><td>' + start + '</td><td>' + end + '</td></tr>';
   }
   $('#display').html(display);
   $('#pre').hide();
